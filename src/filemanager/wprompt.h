c1e6c429dbefce55770a615ae0aa2854ac629869
Ilia Maslakov <il.smind@gmail.com>
2026-08-20 22:19:51 +0200

    filemanager, mcterm: give the new headers the short .h form
    wprompt.h and mcterm_proto.h carried the full license block that belongs
    in a .c file. mc header files use the short "\file / \brief Header:" form,
    the way command.h and panel.h do; switch these two to it.
    
    Signed-off-by: Ilia Maslakov <il.smind@gmail.com>

--- src/filemanager/wprompt.h
+++ src/filemanager/wprompt.h
@@ -1,23 +1,5 @@
-/*
-   Midnight Commander - the shell prompt in front of the command line.
-
-   Copyright (C) 2026
-   Free Software Foundation, Inc.
-
-   This file is part of the Midnight Commander.
-
-   The Midnight Commander is free software: you can redistribute it
-   and/or modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation, either version 3 of the License,
-   or (at your option) any later version.
-
-   The Midnight Commander is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/** \file wprompt.h
+ *  \brief Header: the shell prompt in front of the command line
  */
 
 #ifndef MC__WPROMPT_H
--- src/mcterm/mcterm_proto.h
+++ src/mcterm/mcterm_proto.h
@@ -1,23 +1,5 @@
-/*
-   Midnight Commander - mcterm shell protocol marks.
-
-   Copyright (C) 2026
-   Free Software Foundation, Inc.
-
-   This file is part of the Midnight Commander.
-
-   The Midnight Commander is free software: you can redistribute it
-   and/or modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation, either version 3 of the License,
-   or (at your option) any later version.
-
-   The Midnight Commander is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+/** \file mcterm_proto.h
+ *  \brief Header: mcterm shell protocol marks
  */
 
 #ifndef MC__MCTERM_PROTO_H
